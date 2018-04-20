d315e95a69f7216e260630eaad3ebf2dc8c7ad24
hinayaqub <hina54796@gmail.com>
2018-04-20 18:42:55 +0500

    Real registration form

--- DomainModels/Domain Models/Properties/Resources.Designer.cs
+++ DomainModels/Domain Models/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Domain_Models.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap fast_food {
+            get {
+                object obj = ResourceManager.GetObject("fast_food", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Main_Dish {
             get {
                 object obj = ResourceManager.GetObject("Main_Dish", resourceCulture);
